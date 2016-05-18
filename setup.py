a50d4e03a7990f6e679b2e4c0e9d381c3c47a369
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-05-18 08:51:32 +0200

    changed lint a bit

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+from setuptools.command.test import test as TestCommand
 
 __author__ = 'AllYarnsAreBeautiful'
 __version__ = '0.0.1'
@@ -14,6 +15,11 @@ def read_file_named(file_name):
     file_path = os.path.join(here, file_name)
     with open(file_path) as f:
         return f.read()
+
+def read_filled_lines_from_file_named(file_name):
+    content = read_file_named("requirements-test.txt")
+    lines = content.splitlines()
+    return [line for line in lines if line]
 
 # The base package metadata to be used by both distutils and setuptools
 METADATA = dict(
@@ -29,7 +35,7 @@ METADATA = dict(
 )
 
 # Run tests in setup
-from setuptools.command.test import test as TestCommand
+
 
 class PyTest(TestCommand):
 
@@ -45,36 +51,37 @@ class PyTest(TestCommand):
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
+
 class TestCoverage(PyTest):
     TEST_ARGS = ["--cov=ayab"]
 
 # Extra package metadata to be used only if setuptools is installed
-required_packages = [ package for package in
-                        read_file_named("requirements.txt").splitlines()
-                        if package ]
-required_test_packages = [ package for package in
-                             read_file_named("requirements-test.txt").splitlines()
-                             if package ]
+required_packages = \
+    read_filled_lines_from_file_named("requirements.txt")
+required_test_packages = \
+    read_filled_lines_from_file_named("requirements-test.txt")
+
 SETUPTOOLS_METADATA = dict(
-    install_requires = required_packages,
-    tests_require = required_test_packages,
-    include_package_data = True,
-    classifiers = [
+    install_requires=required_packages,
+    tests_require=required_test_packages,
+    include_package_data=True,
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        'tests': ['test_*.py'],
-    },
-    zip_safe = False,
-    cmdclass = dict(
-        test = PyTest,
-        coverage = TestCoverage,
+    package_data=dict(
+        # If any package contains of these files, include them:
+        tests=['test_*.py'],
+    ),
+    zip_safe=False,
+    cmdclass=dict(
+        test=PyTest,
+        coverage=TestCoverage,
         ),
 )
+
 
 def main():
     # Build the long_description from the README and CHANGES
@@ -88,7 +95,6 @@ def main():
     except ImportError:
         import distutils.core
         distutils.core.setup(**METADATA)
-
 
 if __name__ == '__main__':
     main()
